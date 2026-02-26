f68c3ed68fa37fafb27a65ba422836095a9574b7
TROX25 <pimalogik@gmail.com>
2026-02-26 22:02:34 +0100

    "First Task" showing on the screen

--- src/app/header.ts
+++ src/app/header.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
-    styleUrl: './header.css'
+  styleUrl: './header.css'
 })
 export class Header {
   protected readonly title = 'Angular-app';
